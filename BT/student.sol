f619b0db64b03b4f13bd10ded23d531f5140586d
Shrutika Malve <113254896+ShrutikaM25@users.noreply.github.com>
2024-11-05 21:30:05 +0530

    Update student.sol

--- BT/student.sol
+++ BT/student.sol
@@ -36,12 +36,71 @@ contract StudManagement {
         return stud.length;
     }
 
+    function getAllstud () public view returns (Student [] memory){
+        return stud;
+    }
+
     function getStud(uint _index) public view returns (uint, string memory, uint, string memory) {
         require(_index < stud.length, "Invalid student index!");
 
         Student memory s = stud[_index];
 
         return (s.id, s.name, s.age, s.course);
+    }
+
+    function updateStud(uint _id, string memory _name, uint _age, string memory _course) public {
+    require(StudExists[_id], "Student with that ID does not exist!");
+
+    for (uint i = 0; i < stud.length; i++) {
+        if (stud[i].id == _id) {
+            stud[i].name = _name;
+            stud[i].age = _age;
+            stud[i].course = _course;
+            emit StudentAdded(_id, _name, _age, _course); // Emit the same event to indicate an update
+            return;
+            }
+        }
+    }
+
+    function removeStud(uint _id) public {
+    require(StudExists[_id], "Student with that ID does not exist!");
+
+    for (uint i = 0; i < stud.length; i++) {
+        if (stud[i].id == _id) {
+            for (uint j = i; j < stud.length - 1; j++) {
+                stud[j] = stud[j + 1];
+            }
+            stud.pop();
+            delete StudExists[_id];
+
+            return;
+        }
+    }
+}
+
+// if order is not mandatory we can use below method
+
+function removeStud2(uint _id) public {
+    require(StudExists[_id], "Student with that ID does not exist!");
+
+    for (uint i = 0; i < stud.length; i++) {
+        if (stud[i].id == _id) {
+            // Replace with the last element to avoid shifting elements
+            stud[i] = stud[stud.length - 1];
+            stud.pop();
+
+            // Update the mapping
+            delete StudExists[_id];
+
+            return;
+        }
+    }
+}
+
+
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
     }
 
     fallback() external payable {
